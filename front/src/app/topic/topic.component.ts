c26dc5e2253cd2be8774701ea5d00dbee3d8c204
smahungu <sosthene.mahungu@outlook.com>
2026-07-11 13:16:21 +0200

    chore(front): migrate application to Angular 22

--- front/src/app/topic/topic.component.ts
+++ front/src/app/topic/topic.component.ts
@@ -1,9 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, ChangeDetectionStrategy } from "@angular/core";
 
 @Component({
     selector: 'app-topic',
     templateUrl: './topic.component.html',
     styleUrls: ['./topic.component.css'],
+    changeDetection: ChangeDetectionStrategy.Eager,
     standalone: false
 })
 export class TopicComponent {
