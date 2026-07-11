5fa06e552496154ffaddd0c3b28120db71cc515d
smahungu <sosthene.mahungu@outlook.com>
2026-07-11 12:52:57 +0200

    feat(db): configure PostgreSQL schema with Flyway

---
+++ back/src/main/resources/db/migration/V2__seed_topics.sql
@@ -0,0 +1,6 @@
+INSERT INTO topics (name, description) VALUES
+    ('JavaScript', 'Écosystème JavaScript côté navigateur et serveur.'),
+    ('Java', 'Langage Java et son écosystème.'),
+    ('Python', 'Langage Python et ses usages.'),
+    ('Web3', 'Technologies et protocoles du Web3.'),
+    ('Angular', 'Framework Angular et développement front-end.');
